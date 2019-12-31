4aa977e95436bf668e6f862bfa498ea4782b1d2e
Aaron Patterson <aaron.patterson@gmail.com>
2019-12-31 12:22:14 +0900

    add more constants

--- lib/dwarf/constants.rb
+++ lib/dwarf/constants.rb
@@ -238,5 +238,51 @@ module DWARF
     DW_AT_hi_user                 = 0x3fff
 
     # Table 7.6: Attribute form encodings
+    # Form name | Value | Classes
+    DW_FORM_addr           = 0x01 # address
+    # Reserved             = 0x02
+    DW_FORM_block2         = 0x03 # block
+    DW_FORM_block4         = 0x04 # block
+    DW_FORM_data2          = 0x05 # constant
+    DW_FORM_data4          = 0x06 # constant
+    DW_FORM_data8          = 0x07 # constant
+    DW_FORM_string         = 0x08 # string
+    DW_FORM_block          = 0x09 # block
+    DW_FORM_block1         = 0x0a # block
+    DW_FORM_data1          = 0x0b # constant
+    DW_FORM_flag           = 0x0c # flag
+    DW_FORM_sdata          = 0x0d # constant
+    DW_FORM_strp           = 0x0e # string
+    DW_FORM_udata          = 0x0f # constant
+    DW_FORM_ref_addr       = 0x10 # reference
+    DW_FORM_ref1           = 0x11 # reference
+    DW_FORM_ref2           = 0x12 # reference
+    DW_FORM_ref4           = 0x13 # reference
+    DW_FORM_ref8           = 0x14 # reference
+    DW_FORM_ref_udata      = 0x15 # reference
+    DW_FORM_indirect       = 0x16 # (see Section 7.5.3 on page 203)
+    DW_FORM_sec_offset     = 0x17 # addrptr, lineptr, loclist, loclistsptr, macptr, rnglist, rnglistsptr, stroffsetsptr
+    DW_FORM_exprloc        = 0x18 # exprloc
+    DW_FORM_flag_present   = 0x19 # flag
+    DW_FORM_strx           = 0x1a # string
+    DW_FORM_addrx          = 0x1b # address
+    DW_FORM_ref_sup4       = 0x1c # reference
+    DW_FORM_strp_sup       = 0x1d # string
+    DW_FORM_data16         = 0x1e # constant
+    DW_FORM_line_strp      = 0x1f # string
+    DW_FORM_ref_sig8       = 0x20 # reference
+    DW_FORM_implicit_const = 0x21 # constant
+    DW_FORM_loclistx       = 0x22 # loclist
+    DW_FORM_rnglistx       = 0x23 # rnglist
+    DW_FORM_ref_sup8       = 0x24 # reference
+    DW_FORM_strx1          = 0x25 # string
+    DW_FORM_strx2          = 0x26 # string
+    DW_FORM_strx3          = 0x27 # string
+    DW_FORM_strx4          = 0x28 # string
+    DW_FORM_addrx1         = 0x29 # address
+    DW_FORM_addrx2         = 0x2a # address
+    DW_FORM_addrx3         = 0x2b # address
+    DW_FORM_addrx4         = 0x2c # address
+
   end
 end
