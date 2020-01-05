ca397621e7cace7b5f98501919fe786909b26115
Aaron Patterson <aaron.patterson@gmail.com>
2020-01-05 10:44:19 -0800

    Table 7.13

--- lib/dwarf/constants.rb
+++ lib/dwarf/constants.rb
@@ -496,5 +496,12 @@ module DWARF
     DW_DS_trailing_overpunch = 0x03
     DW_DS_leading_separate   = 0x04
     DW_DS_trailing_separate  = 0x05
+    # Table 7.13: Endianity encodings
+    # Endian code name | Value
+    DW_END_default = 0x00
+    DW_END_big     = 0x01
+    DW_END_little  = 0x02
+    DW_END_lo_user = 0x40
+    DW_END_hi_user = 0xff
   end
 end
