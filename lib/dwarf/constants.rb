40423f984fa8bdab321a35d3f3b8d5a83628a8c6
Aaron Patterson <aaron.patterson@gmail.com>
2020-01-05 11:08:35 -0800

    Table 7.25

--- lib/dwarf/constants.rb
+++ lib/dwarf/constants.rb
@@ -602,5 +602,19 @@ module DWARF
     DW_DEFAULTED_no           = 0x00
     DW_DEFAULTED_in_class     = 0x01
     DW_DEFAULTED_out_of_class = 0x02
+    # Table 7.25: Line number standard opcode encodings
+    # Opcode name | Value
+    DW_LNS_copy               = 0x01
+    DW_LNS_advance_pc         = 0x02
+    DW_LNS_advance_line       = 0x03
+    DW_LNS_set_file           = 0x04
+    DW_LNS_set_column         = 0x05
+    DW_LNS_negate_stmt        = 0x06
+    DW_LNS_set_basic_block    = 0x07
+    DW_LNS_const_add_pc       = 0x08
+    DW_LNS_fixed_advance_pc   = 0x09
+    DW_LNS_set_prologue_end   = 0x0a
+    DW_LNS_set_epilogue_begin = 0x0b
+    DW_LNS_set_isa            = 0x0c
   end
 end
