d5810a066a0dc317e83c63ffd7070548a0275e3a
Aaron Patterson <aaron.patterson@gmail.com>
2020-01-05 11:13:56 -0800

    Table 7.28

--- lib/dwarf/constants.rb
+++ lib/dwarf/constants.rb
@@ -633,5 +633,21 @@ module DWARF
     DW_LNCT_MD5             = 0x05
     DW_LNCT_lo_user         = 0x2000
     DW_LNCT_hi_user         = 0x3fff
+    # Table 7.28: Macro information entry type encodings
+    # Macro information entry type name | Value
+    DW_MACRO_define      = 0x01
+    DW_MACRO_undef       = 0x02
+    DW_MACRO_start_file  = 0x03
+    DW_MACRO_end_file    = 0x04
+    DW_MACRO_define_strp = 0x05
+    DW_MACRO_undef_strp  = 0x06
+    DW_MACRO_import      = 0x07
+    DW_MACRO_define_sup  = 0x08
+    DW_MACRO_undef_sup   = 0x09
+    DW_MACRO_import_sup  = 0x0a
+    DW_MACRO_define_strx = 0x0b
+    DW_MACRO_undef_strx  = 0x0c
+    DW_MACRO_lo_user     = 0xe0
+    DW_MACRO_hi_user     = 0xff
   end
 end
