b6e47feec7b7cec8e568ff73c3977a3b3048578c
Aaron Patterson <aaron.patterson@gmail.com>
2020-01-07 15:00:30 -0800

    we can read dies from DWARF

--- lib/dwarf.rb
+++ lib/dwarf.rb
@@ -16,12 +16,16 @@ module DWARF
   end
 
   class Tag
+    attr_reader :index, :name, :attributes
+
     def initialize index, name, has_children, attributes
       @index        = index
       @name         = name
       @has_children = has_children
       @attributes   = attributes
     end
+
+    def has_children?; @has_children; end
 
     def inspect
       names = @attributes.map { |k,v|
@@ -32,6 +36,117 @@ module DWARF
       "[#{@index}] #{Constants.tag_for(@name)} #{@has_children ? "children" : "no children"}\n" +
         names.map { |k,v| "        #{k.to_s.ljust(maxlen)} #{v}" }.join("\n")
 
+    end
+  end
+
+  class DebugStrings
+    def initialize io, section
+      @io      = io
+      @section = section
+    end
+
+    def string_at offset
+      pos = @io.pos
+      @io.seek @section.offset + offset, IO::SEEK_SET
+      @io.readline("\x00").delete("\x00")
+    ensure
+      @io.seek pos, IO::SEEK_SET
+    end
+  end
+
+  class DIE
+    attr_reader :tag, :attribute, :children
+
+    def initialize tag, attributes, children
+      @tag        = tag
+      @attributes = attributes
+      @children   = children
+    end
+  end
+
+  class DebugInfo
+    def initialize io, section, debug_abbrev
+      @io           = io
+      @section      = section
+      @debug_abbrev = debug_abbrev
+    end
+
+    def compile_units
+      pos = @io.pos
+      tags = @debug_abbrev.tags
+
+      cus = []
+      @io.seek @section.offset, IO::SEEK_SET
+      while @io.pos < @section.offset + @section.size
+        unit_length = @io.read(4).unpack1("L")
+        dwarf_version = @io.read(2).unpack1("S")
+        if dwarf_version != 4
+          raise NotImplementedError, "Only DWARF4 rn"
+        end
+
+        debug_abbrev_offset = @io.read(4).unpack1("L")
+        address_size = @io.readbyte
+        abbrev_code = DWARF.unpackULEB128 @io
+        tag = tags[abbrev_code - 1]
+        cus << parse_die(@io, tags, tag, address_size)
+      end
+      cus
+    ensure
+      @io.seek pos, IO::SEEK_SET
+    end
+
+    private
+
+    def read_children io, tags, address_size
+      children = []
+      loop do
+        abbrev_code = DWARF.unpackULEB128 io
+
+        return children if abbrev_code == 0
+
+        tag = tags[abbrev_code - 1]
+        die = parse_die io, tags, tag, address_size
+        children << die
+      end
+    end
+
+    def parse_die io, tags, tag, address_size
+      attributes = decode tag, address_size, io
+
+      children = if tag.has_children?
+        read_children io, tags, address_size
+      else
+        []
+      end
+      DIE.new tag, attributes, children
+    end
+
+    def decode tag, address_size, io
+      tag.attributes.map do |name, type|
+        case type
+        when Constants::DW_FORM_strp
+          # p strings.string_at io.read(4).unpack1("L")
+          io.read(4).unpack1("L")
+        when Constants::DW_FORM_data1
+          io.readbyte
+        when Constants::DW_FORM_data2
+          io.read(2).unpack1("S")
+        when Constants::DW_FORM_data4
+          io.read(4).unpack1("L")
+        when Constants::DW_FORM_sec_offset
+          io.read(4).unpack1("L")
+        when Constants::DW_FORM_flag_present
+          true
+        when Constants::DW_FORM_addr
+          io.read(address_size).unpack1("Q")
+        when Constants::DW_FORM_exprloc
+          io.read(DWARF.unpackULEB128(io))
+        when Constants::DW_FORM_ref4
+          io.read(4).unpack1("L")
+        else
+          raise "Unhandled type: #{Constants.form_for(type)}"
+        end
+      end
     end
   end
 
--- test/dwarf_test.rb
+++ test/dwarf_test.rb
@@ -8,17 +8,48 @@ module DWARF
     DEBUG_FILE = File.expand_path(File.join(__dir__, debug_file))
 
     def test_debug_abbrev
-      io     = File.open DEBUG_FILE
-      mach_o = MachO.new(io)
+      File.open DEBUG_FILE do |io|
+        mach_o = MachO.new(io)
 
-      section = mach_o.find do |thing|
-        thing.section? && thing.sectname == "__debug_abbrev"
+        section = mach_o.find do |thing|
+          thing.section? && thing.sectname == "__debug_abbrev"
+        end
+
+        debug_abbrev = DWARF::DebugAbbrev.new io, section
+        tags = debug_abbrev.tags
+
+        assert_equal 5, tags.length
       end
+    end
 
-      debug_abbrev = DWARF::DebugAbbrev.new io, section
-      tags = debug_abbrev.tags
+    def test_debug_info
+      File.open DEBUG_FILE do |io|
+        mach_o = MachO.new(io)
 
-      assert_equal 5, tags.length
+        abbrev = mach_o.find do |thing|
+          thing.section? && thing.sectname == "__debug_abbrev"
+        end
+
+        debug_abbrev = DWARF::DebugAbbrev.new io, abbrev
+
+        section_info = mach_o.find do |thing|
+          thing.section? && thing.sectname == "__debug_str"
+        end
+
+        strings = DWARF::DebugStrings.new io, section_info
+
+        info = mach_o.find do |thing|
+          thing.section? && thing.sectname == "__debug_info"
+        end
+
+        debug_info = DWARF::DebugInfo.new io, info, debug_abbrev
+        units = debug_info.compile_units
+
+        assert_equal 1, units.length
+        assert_equal Constants::DW_TAG_compile_unit, units.first.tag.name
+        assert_equal 5, units.first.children.length
+        assert_equal Constants::DW_TAG_subprogram, units.first.children.first.tag.name
+      end
     end
   end
 end
