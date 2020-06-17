bc04b54445c6f540bb0949b56f00db784f3bf461
Aaron Patterson <tenderlove@ruby-lang.org>
2020-06-17 09:11:57 -0700

    seems to do something!

--- lib/dwarf.rb
+++ lib/dwarf.rb
@@ -16,14 +16,18 @@ module DWARF
   end
 
   class Tag
-    attr_reader :index, :name, :attributes
+    attr_reader :index, :type, :attributes
 
-    def initialize index, name, has_children, attributes
+    def initialize index, type, has_children, attributes
       @index        = index
-      @name         = name
+      @type         = type
       @has_children = has_children
       @attributes   = attributes
     end
+
+    class_eval Constants.constants.grep(/^DW_TAG_(.*)$/) { |match|
+      "def #{$1}?; type == Constants::#{match}; end"
+    }.join "\n"
 
     def has_children?; @has_children; end
 
@@ -33,7 +37,7 @@ module DWARF
       }
       maxlen = names.sort_by { |x| x.first.length }.last.first.length
 
-      "[#{@index}] #{Constants.tag_for(@name)} #{@has_children ? "children" : "no children"}\n" +
+      "[#{@index}] #{Constants.tag_for(@type)} #{@has_children ? "children" : "no children"}\n" +
         names.map { |k,v| "        #{k.to_s.ljust(maxlen)} #{v}" }.join("\n")
 
     end
@@ -48,51 +52,100 @@ module DWARF
     def string_at offset
       pos = @io.pos
       @io.seek @section.offset + offset, IO::SEEK_SET
-      @io.readline("\x00").delete("\x00")
+      @io.readline("\x00").b.delete("\x00")
     ensure
       @io.seek pos, IO::SEEK_SET
     end
   end
 
   class DIE
-    attr_reader :tag, :attribute, :children
+    include Enumerable
 
-    def initialize tag, attributes, children
+    attr_reader :tag, :offset, :attributes, :children
+
+    def initialize tag, offset, attributes, children
       @tag        = tag
+      @offset     = offset
       @attributes = attributes
       @children   = children
     end
+
+    def data_member_location
+      at Constants::DW_AT_data_member_location
+    end
+
+    def byte_size
+      at Constants::DW_AT_byte_size
+    end
+
+    def type
+      at Constants::DW_AT_type
+    end
+
+    def name strings
+      tag.attributes.each_with_index do |(name, type), i|
+        if name == Constants::DW_AT_name
+          if type == Constants::DW_FORM_string
+            return attributes[i]
+          else
+            return strings.string_at(attributes[i])
+          end
+        end
+      end
+    end
+
+    def name_offset
+      at Constants::DW_AT_name
+    end
+
+    def each &block
+      yield self
+      children.each { |child| child.each(&block) }
+    end
+
+    private
+
+    def at name
+      idx = tag.attributes.index { |at, _| at == name }
+      idx && attributes[idx]
+    end
   end
+
+  CompilationUnit = Struct.new(:unit_length, :version, :debug_abbrev_offset, :address_size, :die)
 
   class DebugInfo
     def initialize io, section, debug_abbrev
       @io           = io
       @section      = section
       @debug_abbrev = debug_abbrev
+      @head_pos     = @io.pos
     end
 
     def compile_units
-      pos = @io.pos
       tags = @debug_abbrev.tags
 
       cus = []
-      @io.seek @section.offset, IO::SEEK_SET
-      while @io.pos < @section.offset + @section.size
-        unit_length = @io.read(4).unpack1("L")
-        dwarf_version = @io.read(2).unpack1("S")
+      @io.seek @head_pos + @section.offset, IO::SEEK_SET
+      while @io.pos < @head_pos + @section.offset + @section.size
+        unit_length, dwarf_version = @io.read(6).unpack("LS")
         if dwarf_version != 4
-          raise NotImplementedError, "Only DWARF4 rn"
+          raise NotImplementedError, "Only DWARF4 rn #{dwarf_version}"
         end
 
         debug_abbrev_offset = @io.read(4).unpack1("L")
         address_size = @io.readbyte
+        offset = @io.pos - @section.offset
         abbrev_code = DWARF.unpackULEB128 @io
         tag = tags[abbrev_code - 1]
-        cus << parse_die(@io, tags, tag, address_size)
+        cus << CompilationUnit.new(unit_length,
+                                   dwarf_version,
+                                   debug_abbrev_offset,
+                                   address_size,
+                                   parse_die(@io, tags, tag, offset, address_size))
       end
       cus
     ensure
-      @io.seek pos, IO::SEEK_SET
+      @io.seek @head_pos, IO::SEEK_SET
     end
 
     private
@@ -100,17 +153,18 @@ module DWARF
     def read_children io, tags, address_size
       children = []
       loop do
+        offset = io.pos - @section.offset
         abbrev_code = DWARF.unpackULEB128 io
 
         return children if abbrev_code == 0
 
         tag = tags[abbrev_code - 1]
-        die = parse_die io, tags, tag, address_size
+        die = parse_die io, tags, tag, offset, address_size
         children << die
       end
     end
 
-    def parse_die io, tags, tag, address_size
+    def parse_die io, tags, tag, offset, address_size
       attributes = decode tag, address_size, io
 
       children = if tag.has_children?
@@ -118,7 +172,7 @@ module DWARF
       else
         []
       end
-      DIE.new tag, attributes, children
+      DIE.new tag, offset, attributes, children
     end
 
     def decode tag, address_size, io
@@ -143,6 +197,25 @@ module DWARF
           io.read(DWARF.unpackULEB128(io))
         when Constants::DW_FORM_ref4
           io.read(4).unpack1("L")
+        when Constants::DW_FORM_string
+          str = []
+          loop do
+            x = io.readbyte
+            break if x == 0
+            str << x
+          end
+
+          str.pack("C*")
+        when Constants::DW_FORM_flag
+          io.readbyte
+        when Constants::DW_FORM_block1
+          io.read io.readbyte
+        when Constants::DW_FORM_udata
+          DWARF.unpackULEB128 io
+        when Constants::DW_FORM_sdata
+          DWARF.unpackSLEB128 io
+        when Constants::DW_FORM_ref_addr
+          io.read(4).unpack1("L")
         else
           raise "Unhandled type: #{Constants.form_for(type)}"
         end
@@ -154,14 +227,15 @@ module DWARF
     def initialize io, section
       @io      = io
       @section = section
+      @head_pos     = @io.pos
     end
 
     def tags
       @tags ||= begin
-                  @io.seek @section.offset, IO::SEEK_SET
+                  @io.seek @head_pos + @section.offset, IO::SEEK_SET
                   tags = []
                   loop do
-                    break if @io.pos + 1 >= @section.offset + @section.size
+                    break if @io.pos + 1 >= @head_pos + @section.offset + @section.size
                     tags << read_tag
                   end
                   tags
@@ -197,5 +271,24 @@ module DWARF
       end
       shift += 7
     end
+  end
+
+  def self.unpackSLEB128 io
+    result = 0
+    shift = 0
+    size = 64
+
+    loop do
+      byte = io.getbyte
+      result |= ((byte & 0x7F) << shift)
+      shift += 7
+      if (byte >> 7) == 0
+        if shift < size && byte & 0x40
+          result |= (~0 << shift)
+        end
+        break
+      end
+    end
+    result
   end
 end
