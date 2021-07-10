989db5b7dc9e61f25c98f6b31068d7fb89ef358f
Aaron Patterson <tenderlove@ruby-lang.org>
2021-07-10 12:30:03 -0700

    2 locals works

--- test/tenderjit_test.rb
+++ test/tenderjit_test.rb
@@ -68,6 +68,27 @@ class TenderJIT
       assert_equal 1, jit.executed_methods
       assert_equal 0, jit.exits
     end
+
+    def getlocal_wc_0_2 x, y
+      y
+    end
+
+    def test_two_locals
+      jit = TenderJIT.new
+      jit.compile method(:getlocal_wc_0_2)
+      assert_equal 1, jit.compiled_methods
+      assert_equal 0, jit.executed_methods
+      assert_equal 0, jit.exits
+
+      jit.enable!
+      v = getlocal_wc_0_2 "foo", "bar"
+      jit.disable!
+      assert_equal "bar", v
+
+      assert_equal 1, jit.compiled_methods
+      assert_equal 1, jit.executed_methods
+      assert_equal 0, jit.exits
+    end
   end
 
   class HardMethodJIT < Test
