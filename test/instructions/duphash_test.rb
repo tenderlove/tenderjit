5b74ff039b405b8dbb2bfbedc8b4d92a98bcad99
Édipo Féderle <edipofederle@gmail.com>
2021-10-01 10:24:56 -0300

    Add support for dup_hash

--- test/instructions/duphash_test.rb
+++ test/instructions/duphash_test.rb
@@ -4,8 +4,26 @@ require "helper"
 
 class TenderJIT
   class DuphashTest < JITTest
+    def duphash
+      a = {a: 1, b: 2}
+    end
+
     def test_duphash
-      skip "Please implement duphash!"
+      jit.compile method(:duphash)
+      assert_equal 1, jit.compiled_methods
+      assert_equal 0, jit.executed_methods
+      assert_equal 0, jit.exits
+
+      jit.enable!
+      v = duphash
+      jit.disable!
+      assert_equal({a: 1, b: 2}, v)
+
+      assert_equal 1, jit.compiled_methods
+      assert_equal 1, jit.executed_methods
+      assert_equal 0, jit.exits
+    ensure
+      jit.uncompile method(:duphash)
     end
   end
 end
