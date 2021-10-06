32a55650a8ffc5231b891fa5afc6913815f08a8d
Saverio Miroddi <saverio.pub2@gmail.com>
2021-10-06 22:43:57 +0200

    Runtime#if_eq: Swap lhs/rhs when it avoids further register allocations

--- test/runtime_test.rb
+++ test/runtime_test.rb
@@ -27,5 +27,18 @@ class TenderJIT
     def test_if_eq_imm_immnot64
       @rt.if_eq(2 << 0, 2 << 0)
     end
+
+    # See https://github.com/tenderlove/tenderjit/issues/35#issuecomment-934872857
+    #
+    # > The code in main is emitting an extra mov instruction because the lhs is
+    # > an immediate when we could have put the lhs in the rhs and directly used
+    # > the CMP instruction.
+    #
+    # When tackling, update all the invocations, then turn the swap into an
+    # assertion.
+    #
+    def test_if_eq_imm_not_imm
+      skip "Optimize the if_eq invocations"
+    end
   end # class RuntimeTest
 end
