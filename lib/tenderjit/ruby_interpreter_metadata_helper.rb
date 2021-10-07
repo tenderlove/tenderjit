0b932c3ef9e290f8e7a52354a591092cd6369144
Saverio Miroddi <saverio.pub2@gmail.com>
2021-10-07 10:32:33 +0200

    Ruby interpreter fingerprint: Add compiler and compilation information

--- lib/tenderjit/ruby_interpreter_metadata_helper.rb
+++ lib/tenderjit/ruby_interpreter_metadata_helper.rb
@@ -3,7 +3,18 @@ class TenderJIT
     # Returns a fingerprint of the current Ruby interpreter, based the Ruby description.
     #
     def self.fingerprint
-      Digest::MD5.hexdigest(RUBY_DESCRIPTION)[0, 5]
+      # The information is on the verbose side (in particular, CC_VERSION_MESSAGE,
+      # which identifies the compiler version), but it's overall obvious and
+      # comprehensive.
+      #
+      ruby_information = <<~eotxt
+        #{RUBY_DESCRIPTION}
+        #{RbConfig::CONFIG["CC"]}
+        #{RbConfig::CONFIG["CC_VERSION_MESSAGE"]}
+        #{RbConfig::CONFIG["CFLAGS"]}
+      eotxt
+
+      Digest::MD5.hexdigest(ruby_information)[0, 5]
     end
   end
 end
