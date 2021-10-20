184ac0a5fc2ac32d4ad170d4a2ca3b66787ff55f
eileencodes <eileencodes@gmail.com>
2021-10-20 13:46:14 -0400

    Implement opt_mult
    Co-authored-by: Aaron Patterson <tenderlove@ruby-lang.org>
    Co-authored-by: Alisson Santos <alissonbruno.sa@gmail.com>

--- test/instructions/opt_mult_test.rb
+++ test/instructions/opt_mult_test.rb
@@ -4,8 +4,23 @@ require "helper"
 
 class TenderJIT
   class OptMultTest < JITTest
-    def test_opt_mult
-      skip "Please implement opt_mult!"
+    def multiply_literals
+      2 * 2
+    end
+
+    def test_multiply_literals
+      jit.compile method(:multiply_literals)
+      assert_equal 1, jit.compiled_methods
+      assert_equal 0, jit.executed_methods
+
+      jit.enable!
+      v = multiply_literals
+      jit.disable!
+      assert_equal 4, v
+
+      assert_equal 1, jit.compiled_methods
+      assert_equal 1, jit.executed_methods
+      assert_equal 0, jit.exits
     end
   end
 end
