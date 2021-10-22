4da5234bfb0a0b2d5aa7ea0a66d9d3a74f63f32c
Nikita Vasilevsky <nikita.vasilevsky@shopify.com>
2021-10-22 18:50:33 -0400

    Change test skip message for implemented instructions

--- test/instructions/adjuststack_test.rb
+++ test/instructions/adjuststack_test.rb
@@ -5,7 +5,7 @@ require "helper"
 class TenderJIT
   class AdjuststackTest < JITTest
     def test_adjuststack
-      skip "Please implement adjuststack!"
+      skip "adjuststack has been implemented. Please add a test."
     end
   end
 end
