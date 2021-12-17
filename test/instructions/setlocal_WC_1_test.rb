2888d34fcd5c7ad9a5ea73b3548631bcaf80123c
Alisson Santos <alissonbruno.sa@gmail.com>
2021-12-17 18:38:27 +0100

    Add test for handle_setlocal_WC_1

--- test/instructions/setlocal_WC_1_test.rb
+++ test/instructions/setlocal_WC_1_test.rb
@@ -4,8 +4,17 @@ require "helper"
 
 class TenderJIT
   class SetlocalWc1Test < JITTest
+    def setlocal_WC_1(a = 2)
+      proc { a = 3 }
+      a
+    end
+
     def test_setlocal_WC_1
-      skip "setlocal_WC_1 has been implemented. Please add a test."
+      assert_has_insn method(:setlocal_WC_1), insn: :setlocal_WC_1
+
+      # compiled equals 2 because compiled blocks count as compiled method.
+      v = assert_jit method(:setlocal_WC_1), compiled: 2, executed: 1, exits: 0
+      assert_equal 2, v
     end
   end
 end
