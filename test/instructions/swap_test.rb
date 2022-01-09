0efb25bd598eb12a7eeabb14faad9d0c05702df3
Aaron Patterson <tenderlove@ruby-lang.org>
2022-01-09 12:02:43 -0800

    Fix the swap instruction
    The swap instruction wasn't actually writing anything to the stack, so
    this fixed it.  The other problem is that it would pop an item, then
    push the item back on the stack in the reverse order.  This doesn't seem
    like a problem (because that's what `swap` is supposed to do), but each
    item in the temporary stack keeps track of where it needs to be read in
    relation to the base pointer.
    
    Since we just push the same item reference back on the stack, it means
    that subsequent instructions that read from the stack would see the
    wrong offset in relation to the base pointer.

--- test/instructions/swap_test.rb
+++ test/instructions/swap_test.rb
@@ -22,5 +22,41 @@ class TenderJIT
       assert_equal 0, jit.exits
       assert_equal "expression", v
     end
+
+    class Thing
+      attr_reader :m
+
+      def initialize
+        @m = :hi
+      end
+
+      def write= m
+        @m = m
+      end
+    end
+
+    def topswap thing
+      _, thing.write = :foo
+    end
+
+    def test_topswap
+      m = method(:topswap)
+
+      thing = Thing.new
+      expected = topswap(thing)
+
+      thing2 = Thing.new
+
+      jit.compile(m)
+      jit.enable!
+      actual = topswap(thing2)
+      jit.disable!
+
+      assert_equal 2, jit.compiled_methods
+      assert_equal 0, jit.exits
+      assert_equal expected, actual
+      assert_nil thing.m
+      assert_nil thing2.m
+    end
   end
 end
