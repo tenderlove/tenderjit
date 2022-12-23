d9823686dc7092ca7ab07e62951513f4b4f48b3e
Aaron Patterson <tenderlove@ruby-lang.org>
2022-12-23 10:06:05 -0800

    adding some tests

---
+++ test/instructions/anytostring_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "helper"
+
+class TenderJIT
+  class AnytostringTest < JITTest
+    def test_anytostring
+      skip "Please implement anytostring!"
+    end
+  end
+end
---
+++ test/instructions/objtostring_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "helper"
+
+class TenderJIT
+  class ObjtostringTest < JITTest
+    def test_objtostring
+      skip "Please implement objtostring!"
+    end
+  end
+end
